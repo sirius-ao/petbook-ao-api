c403daf51da54a0cbbd5a4c0bcf7bd13f869cdeb
Adilson Futa <adilsonmariafuta@hotmail.com>
2025-06-17 12:24:34 +0200

    Nest tentar conectar de novo após alguns segundos, antes de crashar.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,7 +5,23 @@ import { PrismaClient } from 'generated/prisma';
 export class PrismaService extends PrismaClient implements OnModuleInit {
 
     async onModuleInit() {
-        await this.$connect();
+       // await this.$connect();
+       await this.retryConextion();
+    }
+
+    private async retryConextion(retries = 5, delay =3000): Promise<void>{
+        for (let index = 0; index < retries; index++) {
+         try {
+            await this.$connect();
+            console.log('✅ Prisma conectado com sucesso!');
+            return;
+         } catch (error) {
+            console.error(`❌ Tentativa ${index + 1} de conexão com o banco falhou...`);
+                if (index<retries-1) await new Promise(res => setTimeout(res,delay));
+                else throw new Error('Não foi possível conectar ao banco de dados.');
+         }
+            
+        }
     }
 
     async shutDownHook(app: INestApplication){
