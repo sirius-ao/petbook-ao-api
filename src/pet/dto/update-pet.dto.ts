694e0c931d9d5f90d11411eddaa4da2ea2be68b8
Adilson Futa <adilsonmariafuta@hotmail.com>
2025-07-07 16:01:09 +0200

    mudando os dtos para ter import { PartialType } from '@nestjs/swagger';

--- src/pet/dto/update-pet.dto.ts
+++ src/pet/dto/update-pet.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreatePetDto } from './create-pet.dto';
 
 export class UpdatePetDto extends PartialType(CreatePetDto) {}
