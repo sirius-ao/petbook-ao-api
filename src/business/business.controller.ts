8673c16c71bb4d34554d57beec1c13c4876db8a8
Adilson Futa <adilsonmariafuta@hotmail.com>
2025-07-13 21:46:47 +0200

    documentando e ajustando os metodos do projeto

--- src/business/business.controller.ts
+++ src/business/business.controller.ts
@@ -10,28 +10,42 @@ import {
 import { BusinessService } from './business.service';
 import { CreateBusinessDto } from './dto/create-business.dto';
 import { UpdateBusinessDto } from './dto/update-business.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiNotFoundResponse, ApiBadRequestResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('business')
 @Controller('business')
 export class BusinessController {
   constructor(private readonly businessService: BusinessService) {}
 
+  @ApiOperation({summary: 'Cadastro e gestão de clínicas/petshops'})
+  @ApiResponse({status: 201, description: '	Cadastrar novo Negocio'})
+  @ApiBadRequestResponse({description: 'Bad Payload send'})
+  @ApiResponse({status: 403, description: 'Forbiden'})
   @Post()
   create(@Body() createBusinessDto: CreateBusinessDto) {
     return this.businessService.create(createBusinessDto);
   }
 
+
+ @ApiOperation({summary: 'Listar negócios'})
+  @ApiOkResponse({description:'Listar negócios', type: CreateBusinessDto})
+  @ApiNotFoundResponse({description:'Not Found'})
   @Get()
   findAll() {
     return this.businessService.findAll();
   }
 
+  @ApiOperation({summary: 'Ver detalhes negocios'})
+  @ApiOkResponse({description:'Ver detalhes negocios', type: CreateBusinessDto})
+  @ApiNotFoundResponse({description:'Not Found'})
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.businessService.findOne(id);
   }
 
+   @ApiOperation({summary: 'Atualizar informações'})
+    @ApiOkResponse({description:'Atualizar informações', type: CreateBusinessDto})
+    @ApiNotFoundResponse({description:'Not Found'})
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -40,6 +54,9 @@ export class BusinessController {
     return this.businessService.update(id, updateBusinessDto);
   }
 
+   @ApiOperation({summary: 'Deletar negócios'})
+    @ApiOkResponse({description:'Deletar negócios ', type: CreateBusinessDto})
+    @ApiNotFoundResponse({description:'Not Found'})
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.businessService.remove(id);
--- src/pet/pet.controller.ts
+++ src/pet/pet.controller.ts
@@ -10,36 +10,54 @@ import {
 import { PetService } from './pet.service';
 import { CreatePetDto } from './dto/create-pet.dto';
 import { UpdatePetDto } from './dto/update-pet.dto';
-import { ApiTags } from '@nestjs/swagger';
-
+import { ApiTags, ApiOperation, ApiBadRequestResponse, ApiResponse, ApiOkResponse, ApiNotFoundResponse } from '@nestjs/swagger';
 
 @ApiTags('pet')
 @Controller('pet')
 export class PetController {
   constructor(private readonly petService: PetService) {}
 
+  @ApiOperation({ summary: 'Registrar pet' })
+  @ApiResponse({ status: 201, description: 'Registrar pet' })
+  @ApiBadRequestResponse({ description: 'Bad Payload send' })
+  @ApiResponse({ status: 403, description: 'Forbiden' })
   @Post()
   create(@Body() createPetDto: CreatePetDto) {
     return this.petService.create(createPetDto);
   }
 
+  @ApiOperation({ summary: 'Listar pets' })
+  @ApiOkResponse({ description: 'Listar pets', type: CreatePetDto })
+  @ApiNotFoundResponse({ description: 'Not Found' })
   @Get()
   findAll() {
     return this.petService.findAll();
   }
 
+  @ApiOperation({ summary: 'Detalhes do pet' })
+  @ApiOkResponse({ description: 'Detalhes do pet', type: CreatePetDto })
+  @ApiNotFoundResponse({ description: 'Not Found' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.petService.findOne(id); // ✅ ID é string
   }
 
+  @ApiOperation({ summary: 'Atualizar pets' })
+  @ApiOkResponse({ description: 'Atualizar pet', type: CreatePetDto })
+  @ApiNotFoundResponse({ description: 'Not Found' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePetDto: UpdatePetDto) {
     return this.petService.update(id, updatePetDto); // ✅ ID é string
   }
 
+  @ApiOperation({ summary: 'Excluir pet' })
+  @ApiOkResponse({ description: 'Excluir pet', type: CreatePetDto })
+  @ApiNotFoundResponse({ description: 'Not Found' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.petService.remove(id); // ✅ ID é string
   }
+
+  // GET	/clients/:id/pets	Listar pets de um dono específico
+  // falta criar esse request
 }
