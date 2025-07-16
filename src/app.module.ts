e2647702ad5241d03c4c0ca9eb8fce74e4f30743
Adilson Futa <adilsonmariafuta@hotmail.com>
2025-07-16 18:30:48 +0200

    reestruturando o projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,38 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
-import { BusinessModule } from './business/business.module';
-import { ClientModule } from './client/client.module';
-import { PetModule } from './pet/pet.module';
-import { AppointmentModule } from './appointment/appointment.module';
-import { MedicalRecordModule } from './medical-record/medical-record.module';
-import { ProductModule } from './product/product.module';
-import { SaleModule } from './sale/sale.module';
-import { SaleItemModule } from './sale-item/sale-item.module';
-import { ServiceModule } from './service/service.module';
-import { AffiliateModule } from './affiliate/affiliate.module';
-import { AffiliateReferralModule } from './affiliate-referral/affiliate-referral.module';
 import { CustomModule } from './custom/custom.module';
 import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
+import { DomainModule } from './domain/domain.module';
 
 @Module({
   imports: [
     PrismaModule,
-    UserModule,
-    BusinessModule,
-    ClientModule,
-    PetModule,
-    AppointmentModule,
-    MedicalRecordModule,
-    ProductModule,
-    SaleModule,
-    SaleItemModule,
-    ServiceModule,
-    AffiliateModule,
-    AffiliateReferralModule,
     CustomModule,
-    AuthModule
+    AuthModule,
+    CommonModule,
+    DatabaseModule,
+    ConfigModule,
+    DomainModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/domain/domain.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { BusinessModule } from './business/business.module';
+import { ClientModule } from './client/client.module';
+import { PetModule } from './pet/pet.module';
+import { AppointmentModule } from './appointment/appointment.module';
+import { MedicalRecordModule } from './medical-record/medical-record.module';
+import { ProductModule } from './product/product.module';
+import { SaleModule } from './sale/sale.module';
+import { SaleItemModule } from './sale-item/sale-item.module';
+import { ServiceModule } from './service/service.module';
+import { AffiliateModule } from './affiliate/affiliate.module';
+import { AffiliateReferralModule } from './affiliate-referral/affiliate-referral.module';
+
+@Module({
+    imports:[    
+        UserModule,
+        BusinessModule,
+        ClientModule,
+        PetModule,
+        AppointmentModule,
+        MedicalRecordModule,
+        ProductModule,
+        SaleModule,
+        SaleItemModule,
+        ServiceModule,
+        AffiliateModule,
+        AffiliateReferralModule,],
+    controllers:[],
+    providers:[DomainModule],
+    exports:[DomainModule]
+})
+export class DomainModule {}
