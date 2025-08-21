ccab96ef75fd77dd4a908e9f681f6e48484526fd
denypro <dntepro@gmail.com>
2025-08-21 13:44:09 +0100

    ja

--- src/domain/client/notification/whatsapp.module.ts
+++ src/domain/client/notification/whatsapp.module.ts
@@ -1,9 +1,10 @@
 // src/whatsapp/whatsapp.module.ts
 import { Module } from "@nestjs/common";
 import { WhatsappService } from "./whatsapp.service";
+import { OpenAIService } from "../../openai/openai.service";
 
 @Module({
-  providers: [WhatsappService],
+  providers: [WhatsappService, OpenAIService],
   exports: [WhatsappService],
 })
 export class WhatsappModule {}
